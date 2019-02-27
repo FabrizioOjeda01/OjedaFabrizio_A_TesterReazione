1ef319412f01e60512d870ac0e3394797782fc02
Fabrizio <fabrizioojeda@hotmail.it>
2019-02-27 13:34:25 +0100

    27-02-19
    Aggiunta variabili

--- TestReazione/TestReazione.ino
+++ TestReazione/TestReazione.ino
@@ -1,5 +1,16 @@
+              //variabili input
+int btn_Inizio;
+int btn_Led;
+int btn_beep;
+              //variabili OUTPUT
+int ledInizio;
+int ledVerde;
+int ledRosso;
+int beep;
+
 void setup() {
   // put your setup code here, to run once:
+lcd.begin(16, 2);
 
 }
 
