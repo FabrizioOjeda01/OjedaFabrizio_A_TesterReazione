ad1f817b02a10674820830bd7c3dea93b3d98f6e
Fabrizio <fabrizioojeda@hotmail.it>
2019-03-02 08:43:16 +0100

    02/03/19
    Setup finito

--- TestReazione/TestReazione.ino
+++ TestReazione/TestReazione.ino
@@ -11,7 +11,23 @@ int beep;
 void setup() {
   // put your setup code here, to run once:
 lcd.begin(16, 2);
+pinMode(btn_Inizio , INPUT);
+pinMode(btn_Led, INPUT);
+pinMode(btn_beep , INPUT);
 
+pinMode(ledInizio,OUTPUT);
+pinMode(ledVerde,OUTPUT);
+pinMode(ledRosso,OUTPUT);
+pinMode(beep,OUTPUT);
+
+btn_Inizio = 1;
+btn_Led    = 2;
+btn_beep   = 3;
+
+ledInizio  = 8;
+ledVerde   = 9;
+ledRosso   = 10;
+beep       = 11;
 }
 
 void loop() {
