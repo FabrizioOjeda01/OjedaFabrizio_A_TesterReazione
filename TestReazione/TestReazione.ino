0aff9440e6d9d7f8e8ea377a061e85e9ce0713b0
fabrizio <fabrizioojeda@hotmail.it>
2019-03-14 15:18:34 +0100

    14-03-19
    Aggiunto l'inzio del test, manca ancora il metodo per il calcolo dei tempi di reazione e determinare quindi poi l'esito. Manca da settare l'LCD. Tutte le variabili sono dichiarate.

--- TestReazione/TestReazione.ino
+++ TestReazione/TestReazione.ino
@@ -1,16 +1,22 @@
-              //variabili input
+#include <LiquidCrystal_I2C.h>
+
+              // variabili input
 int btn_Inizio;
 int btn_Led;
-int btn_beep;
-              //variabili OUTPUT
-int ledInizio;
-int ledVerde;
-int ledRosso;
-int beep;
+int btn_Suono;
+              // variabili OUTPUT
+int led_Inizio;
+int led_Verde;
+int led_Rosso;
+int suono;
+             //  variabili 
+int esito1;
+int esito2;
+int test1;
+int test2;
 
 void setup() {
-  // put your setup code here, to run once:
-lcd.begin(16, 2);
+
 pinMode(btn_Inizio , INPUT);
 pinMode(btn_Led, INPUT);
 pinMode(btn_beep , INPUT);
@@ -28,9 +34,26 @@ ledInizio  = 8;
 ledVerde   = 9;
 ledRosso   = 10;
 beep       = 11;
+
+test1      = 750;
+test2      = 750;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Finché non verrà rilevata la pressione del bottone di inizio, non inizierà nulla
+  while(digitalRead(btn_Inizio) == LOW){};
+  digitalWrite (led_Verde LOW);
+  digitalWrite (led_Rosso LOW);
+  lcd.clear();
 
+  //Richiamo del metodo per il calcolo del tempo di reazione impiegato sia per il led che per il buzzer
+  /*esito1 = inserire metodo con variabili input
+    esito2= inserire meotodo con variabili input
+   */
+   //Esito finale, led verde= test superato, led rosso = test non superato
+   if(esito1 <= test1 && esito2 <= test2){
+    digitalWrite (led_Verde HIGH);
+   }else{
+    digitalWrite (led_Rosso HIGH);
+   }
 }
