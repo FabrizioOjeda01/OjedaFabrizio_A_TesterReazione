935804f14d0694c04ff032968473a207b40329f4
Fabrizio <fabrizioojeda@hotmail.it>
2019-03-16 08:50:50 +0100

    16-03-19
    Aggiunta dell'LCD e testato.

--- TestReazione/TestReazione.ino
+++ TestReazione/TestReazione.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
               // variabili input
 int btn_Inizio;
@@ -16,44 +17,46 @@ int test1;
 int test2;
 
 void setup() {
+lcd.init();
+lcd.backlight();
 
 pinMode(btn_Inizio , INPUT);
-pinMode(btn_Led, INPUT);
-pinMode(btn_beep , INPUT);
+pinMode(btn_Led,     INPUT);
+pinMode(btn_Suono ,  INPUT);
 
-pinMode(ledInizio,OUTPUT);
-pinMode(ledVerde,OUTPUT);
-pinMode(ledRosso,OUTPUT);
-pinMode(beep,OUTPUT);
+pinMode(led_Inizio,  OUTPUT);
+pinMode(led_Verde,   OUTPUT);
+pinMode(led_Rosso,   OUTPUT);
+pinMode(suono,       OUTPUT);
 
-btn_Inizio = 1;
-btn_Led    = 2;
-btn_beep   = 3;
+btn_Inizio  = 1;
+btn_Led     = 2;
+btn_Suono   = 3;
 
-ledInizio  = 8;
-ledVerde   = 9;
-ledRosso   = 10;
-beep       = 11;
+led_Inizio  = 8;
+led_Verde   = 9;
+led_Rosso   = 10;
+suono       = 11;
 
-test1      = 750;
-test2      = 750;
+test1       = 750;
+test2       = 750;
 }
 
 void loop() {
   //Finché non verrà rilevata la pressione del bottone di inizio, non inizierà nulla
   while(digitalRead(btn_Inizio) == LOW){};
-  digitalWrite (led_Verde LOW);
-  digitalWrite (led_Rosso LOW);
+  digitalWrite (led_Verde, LOW);
+  digitalWrite (led_Rosso, LOW);
   lcd.clear();
 
   //Richiamo del metodo per il calcolo del tempo di reazione impiegato sia per il led che per il buzzer
   /*esito1 = inserire metodo con variabili input
-    esito2= inserire meotodo con variabili input
-   */
-   //Esito finale, led verde= test superato, led rosso = test non superato
-   if(esito1 <= test1 && esito2 <= test2){
-    digitalWrite (led_Verde HIGH);
-   }else{
-    digitalWrite (led_Rosso HIGH);
-   }
+    esito2= inserire meotodo con variabili input */
+   
+  //Esito finale, led verde= test superato, led rosso = test non superato
+  if(esito1 <= test1 && esito2 <= test2){
+  digitalWrite (led_Verde, HIGH);
+  }else{
+  digitalWrite (led_Rosso, HIGH);
+  }
 }
